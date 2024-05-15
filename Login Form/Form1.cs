b00f462c8c5d3b3cc9dbdb97510496273f53ce0b
mandiseli <mandiseli@outlook.com>
2024-05-15 08:19:38 +0200

    Add project files.

---
+++ Login Form/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("");
+            string query = "SELECT COUNT(*) FROM loginapp WHERE username=@username AND password=@password";
+            con.Open();
+            SqlCommand cmd =new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@username", txtUser.Text);
+            cmd.Parameters.AddWithValue("@password", txtPass.Text);
+            int count = (int)cmd.ExecuteScalar();
+            con.Close();
+            if (count > 0) 
+            {
+                MessageBox.Show("Login successful", "info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("error in login");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            txtPass.PasswordChar = checkBox1.Checked ? '\0' : '^';
+        }
+    }
+}
